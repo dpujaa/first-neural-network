a81298d92bdb6d498be3734bab9f22aea85834f2
dpujaa <dpujaa@gmail.com>
2025-08-02 23:11:37 -0700

    training using mse

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -65,3 +65,19 @@ print(network.feedforward(x))
 
 # should print the output of the neural network after applying the sigmoid activation function = 0.7216325609518421
 
+
+#3) Training the Neural Network
+
+# using MSE to calculate the loss
+
+def mse_loss(y_true, y_pred):
+    return ((y_true- y_pred)** 2).mean()
+
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+# print(mse_loss(y_true, y_pred)) # 0.5
+
+
+#4) Backpropagation -- 
